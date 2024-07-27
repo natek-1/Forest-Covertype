0c42f00ee6cdbf49b0060eeb1691c2568ef9e905
Nathan Gabriel <nategabrielk@icloud.com>
2024-07-26 20:06:36 -0400

    setup file for data ingestion

---
+++ src/forestCover/config/__init__.py
@@ -0,0 +1,19 @@
+import os
+from from_root import from_root
+from pathlib import Path
+
+## file names
+
+FILE_NAME: str = "covertype.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+SCHEMA_FILE_PATH: Path = os.path.join("config", "schema.yaml")
+
+
+ARTIFACT_DIR = os.path.join(from_root(), "artifacts")
+
+## Data Ingestion
+
+DATA_INGESTION_DIR: str = "DataIngestion"
+DATA_INGESTION_FEATURE_STORE: str = "feature_store"
+DATA_INGESTION_TRAIN_TEST_SPLIT: float = 0.2
---
+++ src/forestCover/entity/config_entity.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from from_root import from_root
+import os
+from pathlib import Path
+from forestCover.constants import (ARTIFACT_DIR, DATA_INGESTION_DIR, DATA_INGESTION_FEATURE_STORE, TRAIN_FILE_NAME,
+                                    TEST_FILE_NAME, DATA_INGESTION_TRAIN_TEST_SPLIT)
+
+
+@dataclass
+class DataIngestionConfig:
+    data_ingestion_dir: Path = os.path.join(ARTIFACT_DIR, DATA_INGESTION_DIR)
+    feature_store_path: Path = os.path.join(data_ingestion_dir, DATA_INGESTION_FEATURE_STORE)
+    train_file_path: Path = os.path.join(data_ingestion_dir, TRAIN_FILE_NAME)
+    test_file_path: Path = os.path.join(data_ingestion_dir, TEST_FILE_NAME)
+    train_test_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT
